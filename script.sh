814d63ccb1ba7168311472d3c06ff0c86c13338d
hamzabbasi0 <105022437+hamzabbasi0@users.noreply.github.com>
2022-05-08 04:20:38 -0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,27 @@
+usage() {echo "usage:bash $0 -p < > "1>&2; exit 1; }
+if [$# -eq 0 ];
+then
+	usage
+else
+	pwd=""
+	while getopts 'p:h' c;
+	do
+	 case $c in
+	   p) pwd=$OPTARG;;
+	   h) usage;;
+	   *) echo "need to specify password in command";;
+	 esac
+	done
+fi
+
+if [[-n $pwd ]]; then
+	HOST="locahost"
+	user="wpadmin"
+	password="Password!@#"
+	mysql -u root -p$pwd <<EOF
+	CREATE DATABASE wordpress;
+	GRANT ALL PRIVILEGES ON wordpress.* TO "${user}"@"${HOST}" IDENTIFIED BY "${password};"
+	FLUSH PRIVILEGES;
+	exit;
+EOF
+fi
